71140874735b394a4145ccf24399103fff889f6f
Joao Victor Camargo Silva <jc7155004@gmail.com>
2024-05-02 12:58:00 -0300

    Initial commit

---
+++ src/js/EmblaCarousel.tsx
@@ -0,0 +1,95 @@
+import React, { useState, useEffect, useCallback } from 'react'
+import { EmblaOptionsType } from 'embla-carousel'
+import useEmblaCarousel from 'embla-carousel-react'
+import { Thumb } from './EmblaCarouselThumbsButton'
+import {
+  PrevButton,
+  NextButton,
+  usePrevNextButtons
+} from './EmblaCarouselArrowButtons'
+import img1 from '../images/Property1.png'
+import img2 from '../images/collage.png'
+import img3 from '../images/collage2.png'
+
+type PropType = {
+  slides: number[]
+  options?: EmblaOptionsType
+}
+
+const EmblaCarousel: React.FC<PropType> = ({ slides, options }) => {
+  const [selectedIndex, setSelectedIndex] = useState(0)
+  const [emblaMainRef, emblaMainApi] = useEmblaCarousel(options)
+  const [emblaThumbsRef, emblaThumbsApi] = useEmblaCarousel({
+    containScroll: 'keepSnaps',
+    dragFree: true,
+    slidesToScroll: 7
+  })
+
+  const {
+    prevBtnDisabled,
+    nextBtnDisabled,
+    onPrevButtonClick,
+    onNextButtonClick
+  } = usePrevNextButtons(emblaMainApi, slides.length)
+
+  const imageUrls = [img1, img3, img2, img1, img1, img1, img1, img1]
+
+  const onThumbClick = useCallback(
+    (index: number) => {
+      if (!emblaMainApi || !emblaThumbsApi) return
+      emblaMainApi.scrollTo(index)
+      setSelectedIndex(index)
+    },
+    [emblaMainApi, emblaThumbsApi]
+  )
+
+  const onSelect = useCallback(() => {
+    if (!emblaMainApi || !emblaThumbsApi) return
+    const selected = emblaMainApi.selectedScrollSnap()
+    setSelectedIndex(selected)
+    emblaThumbsApi.scrollTo(Math.max(0, selected - 1))
+  }, [emblaMainApi, emblaThumbsApi])
+
+  useEffect(() => {
+    if (emblaMainApi && emblaThumbsApi) {
+      onSelect()
+      emblaMainApi.on('select', onSelect)
+      emblaMainApi.on('reInit', onSelect)
+    }
+  }, [emblaMainApi, emblaThumbsApi, onSelect])
+
+  return (
+    <section className="embla">
+      <PrevButton onClick={onPrevButtonClick} disabled={prevBtnDisabled} />
+      <div className="embla__viewport" ref={emblaMainRef}>
+        <div className="embla__container">
+          {slides.map((index) => (
+            <div
+              className="embla__slide"
+              key={index}
+              style={{ backgroundImage: `url(${imageUrls[index]})` }}
+            />
+          ))}
+        </div>
+      </div>
+      <div className="embla-thumbs">
+        <div className="embla-thumbs__viewport" ref={emblaThumbsRef}>
+          <div className="embla-thumbs__container">
+            {slides.map((index) => (
+              <Thumb
+                key={index}
+                onClick={() => onThumbClick(index)}
+                selected={index === selectedIndex}
+                index={index}
+                imageUrl={imageUrls[index]}
+              />
+            ))}
+          </div>
+        </div>
+      </div>
+      <NextButton onClick={onNextButtonClick} disabled={nextBtnDisabled} />
+    </section>
+  )
+}
+
+export default EmblaCarousel
---
+++ src/js/EmblaCarouselThumbsButton.tsx
@@ -0,0 +1,34 @@
+import React from 'react'
+
+type PropType = {
+  selected: boolean
+  index: number
+  onClick: () => void
+  imageUrl: string // URL of the image for the thumbnail
+}
+
+export const Thumb: React.FC<PropType> = ({
+  selected,
+  index,
+  onClick,
+  imageUrl
+}) => {
+  return (
+    <div
+      className={'embla-thumbs__slide'.concat(
+        selected ? ' embla-thumbs__slide--selected' : ''
+      )}
+    >
+      <button
+        onClick={onClick}
+        type="button"
+        className="embla-thumbs__slide__number"
+        style={{
+          backgroundImage: `url(${imageUrl})`,
+          backgroundSize: 'cover',
+          backgroundPosition: 'center'
+        }}
+      />
+    </div>
+  )
+}
