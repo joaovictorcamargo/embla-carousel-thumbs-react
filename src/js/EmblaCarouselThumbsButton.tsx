65776b62b546adb6f2d0a74c8d9d1a6f27959a3d
João Victor Camargo Silva <JoaoVictor.Camargo@energage.com>
2024-05-03 15:30:38 -0300

    feat: thumb border

--- src/js/EmblaCarouselThumbsButton.tsx
+++ src/js/EmblaCarouselThumbsButton.tsx
@@ -7,6 +7,13 @@ type PropType = {
 }
 
 export const Thumb: React.FC<PropType> = ({ selected, onClick, imageUrl }) => {
+  const buttonStyle = {
+    backgroundImage: `url(${imageUrl})`,
+    backgroundSize: 'cover',
+    backgroundPosition: 'center',
+    border: selected ? '4px solid var(--Purple-400, #8540EF)' : 'none'
+  }
+
   return (
     <div
       className={'embla-thumbs__slide'.concat(
@@ -17,11 +24,7 @@ export const Thumb: React.FC<PropType> = ({ selected, onClick, imageUrl }) => {
         onClick={onClick}
         type="button"
         className="embla-thumbs__slide__number"
-        style={{
-          backgroundImage: `url(${imageUrl})`,
-          backgroundSize: 'cover',
-          backgroundPosition: 'center'
-        }}
+        style={buttonStyle}
         tabIndex={3}
       />
     </div>
