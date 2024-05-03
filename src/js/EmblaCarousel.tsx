e02ff79a2315010c93a078e5e4afd6d1035ac81b
João Victor Camargo Silva <JoaoVictor.Camargo@energage.com>
2024-05-03 16:40:41 -0300

    feat: get principal color

--- src/js/EmblaCarousel.tsx
+++ src/js/EmblaCarousel.tsx
@@ -18,6 +18,7 @@ type PropType = {
 
 const EmblaCarousel: React.FC<PropType> = ({ slides, options }) => {
   const [selectedIndex, setSelectedIndex] = useState(0)
+  const [backgroundColor, setBackgroundColor] = useState('red')
   const [emblaMainRef, emblaMainApi] = useEmblaCarousel(options)
   const [emblaThumbsRef, emblaThumbsApi] = useEmblaCarousel({
     containScroll: 'keepSnaps',
@@ -58,46 +59,73 @@ const EmblaCarousel: React.FC<PropType> = ({ slides, options }) => {
     }
   }, [emblaMainApi, emblaThumbsApi, onSelect])
 
+  useEffect(() => {
+    const canvas = document.createElement('canvas')
+    const context = canvas.getContext('2d')
+
+    if (context !== null) {
+      const image = new Image()
+      image.crossOrigin = 'Anonymous'
+      image.src = imageUrls[selectedIndex]
+      image.onload = () => {
+        canvas.width = image.width
+        canvas.height = image.height
+        context.drawImage(image, 0, 0, image.width, image.height)
+
+        const centerX = Math.floor(image.width / 2)
+        const centerY = Math.floor(image.height / 2)
+        const pixelData = context.getImageData(centerX, centerY, 1, 1).data
+
+        const averageColor = `rgb(${pixelData[0]}, ${pixelData[1]}, ${pixelData[2]})`
+        setBackgroundColor(averageColor)
+      }
+    } else {
+      console.error('Canvas context is null')
+    }
+  }, [selectedIndex, imageUrls])
+
   return (
-    <section className="embla">
-      <PrevButton
-        onClick={onPrevButtonClick}
-        disabled={prevBtnDisabled}
-        tabIndex={selectedIndex === 0 ? -1 : 1}
-      />
-      <div className="embla__viewport" ref={emblaMainRef}>
-        <div className="embla__container">
-          {slides.map((index) => (
-            <div className="embla__slide" key={index}>
-              <img
-                src={imageUrls[index]}
-                alt={`Slide ${index}`}
-                style={{ width: '100%', height: '100%', objectFit: 'cover' }}
-              />
-            </div>
-          ))}
-        </div>
-      </div>
-      <div className="embla-thumbs" role="tablist" aria-label="Slides">
-        <div className="embla-thumbs__viewport" ref={emblaThumbsRef}>
-          <div className="embla-thumbs__container">
+    <div style={{ backgroundColor }}>
+      <section className="embla">
+        <PrevButton
+          onClick={onPrevButtonClick}
+          disabled={prevBtnDisabled}
+          tabIndex={selectedIndex === 0 ? -1 : 1}
+        />
+        <div className="embla__viewport" ref={emblaMainRef}>
+          <div className="embla__container">
             {slides.map((index) => (
-              <Thumb
-                key={index}
-                onClick={() => onThumbClick(index)}
-                selected={index === selectedIndex}
-                imageUrl={imageUrls[index]}
-              />
+              <div className="embla__slide" key={index}>
+                <img
+                  src={imageUrls[index]}
+                  alt={`Slide ${index}`}
+                  style={{ width: '100%', height: '100%', objectFit: 'cover' }}
+                />
+              </div>
             ))}
           </div>
         </div>
-      </div>
-      <NextButton
-        onClick={onNextButtonClick}
-        disabled={nextBtnDisabled}
-        tabIndex={selectedIndex === 0 ? 1 : 2}
-      />
-    </section>
+        <div className="embla-thumbs" role="tablist" aria-label="Slides">
+          <div className="embla-thumbs__viewport" ref={emblaThumbsRef}>
+            <div className="embla-thumbs__container">
+              {slides.map((index) => (
+                <Thumb
+                  key={index}
+                  onClick={() => onThumbClick(index)}
+                  selected={index === selectedIndex}
+                  imageUrl={imageUrls[index]}
+                />
+              ))}
+            </div>
+          </div>
+        </div>
+        <NextButton
+          onClick={onNextButtonClick}
+          disabled={nextBtnDisabled}
+          tabIndex={selectedIndex === 0 ? 1 : 2}
+        />
+      </section>
+    </div>
   )
 }
 
